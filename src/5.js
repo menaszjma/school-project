af947d30d514a0197b91e5eabaee4b03bc252f67
menaszjma <menaszjma@gmx.com>
2025-03-07 01:04:46 +0000

    Add 5.js

---
+++ src/5.js
@@ -0,0 +1,13 @@
+let students = [];
+
+function addStudent(name) {
+  students.push(name);
+}
+
+function getStudents() {
+  return students;
+}
+
+addStudent("John Doe");
+addStudent("Jane Smith");
+addStudent("Bob Brown");
